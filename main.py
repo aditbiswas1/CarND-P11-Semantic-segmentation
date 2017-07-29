4f01b48fa0332cbc701cf9a2e7c52da5dbe74fa9
Adit Biswas <adit.biswas@grofers.com>
2017-07-29 19:32:45 +0530

    bugfix: dictionary keys

--- main.py
+++ main.py
@@ -111,10 +111,10 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     for epoch in range(epochs):
         for input_image, correct_label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss], 
-                                     feed_dict = {input_image: input_image, 
-                                                  correct_label: correct_label, 
-                                                  keep_prob: 0.80, 
-                                                  learning_rate: 0.00005})
+                                     feed_dict = {'input_image': input_image, 
+                                                  'correct_label': correct_label, 
+                                                  'keep_prob': 0.80, 
+                                                  'learning_rate': 0.00005})
             print(count)
             if count % 2 == 0: 
                 print("Epoch {}/{}...".format(epoch, epochs),
